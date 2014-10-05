d5e80478b1e9fd5a734eebcabbe7fe5b499cc647
block_given? <block_given@outlook.com>
2014-10-05 14:56:33 +0900

    Add brewfile.sh

---
+++ brewfile.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+brew update
+brew upgrade
+
+brew install go
+brew install hub
+brew install rbenv
+brew install readline
+brew install ruby-build
+brew install tig
+brew install zsh
+
+brew tap motemen/ghq
+brew install ghq
+
+brew tap peco/peco
+brew install peco
+
+brew install caskroom/cask/brew-cask
+brew cask install atom
+brew cask install dropbox
+brew cask install google-japanese-ime
+brew cask install iterm2
+brew cask install yorufukurou
+
+brew cleanup
