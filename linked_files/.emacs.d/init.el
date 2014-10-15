8977451bf48b983020352d7b3f181af4819a8c5c
block_given? <block_given@outlook.com>
2014-10-16 01:57:27 +0900

    add .emacs.d

---
+++ linked_files/.emacs.d/init.el
@@ -0,0 +1,5 @@
+(keyboard-translate ?\C-h ?\C-?)
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
