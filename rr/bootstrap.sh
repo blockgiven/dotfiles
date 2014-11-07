462c597f45eeba3982810c3a2be2934cf05bac67
block_given? <block_given@outlook.com>
2014-11-07 09:29:14 +0900

    clone ruby repository

--- rr/bootstrap.sh
+++ rr/bootstrap.sh
@@ -5,3 +5,8 @@ fi
 
 apt-get update
 apt-get install -y git
+
+if [ ! -d /home/vagrant/ruby ]; then
+  sudo -u vagrant git clone https://github.com/ruby/ruby.git /home/vagrant/ruby
+fi
+sudo -u vagrant sh -c 'cd /home/vagrant/ruby && git fetch'
