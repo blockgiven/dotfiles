0882a0803bf5e5b7e5e9c1c6164de030c99ac7b1
block_given? <block_given@outlook.com>
2014-11-09 12:01:48 +0900

    use melpa packages

--- linked_files/.emacs.d/init.el
+++ linked_files/.emacs.d/init.el
@@ -9,6 +9,7 @@
 
 (when (>= emacs-major-version 24)
   (require 'package)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
   (package-initialize)
   (package-refresh-contents)
   )
