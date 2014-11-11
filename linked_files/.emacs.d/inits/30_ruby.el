36c91fe1c7581446fc5b52dbedcfb6440d17550a
block_given? <block_given@outlook.com>
2014-11-11 22:42:44 +0900

    install rcodetools

--- linked_files/.emacs.d/inits/30_ruby.el
+++ linked_files/.emacs.d/inits/30_ruby.el
@@ -10,3 +10,5 @@
 (setq inf-ruby-default-implementation "pry")
 (setq inf-ruby-eval-binding "Pry.toplevel_binding")
 (add-hook 'inf-ruby-mode-hook 'ansi-color-for-comint-mode-on)
+
+(require 'rcodetools)
